afca1a36cb0342710641c12185b2f4afbbc04fc3
richa <richardnham2000@gmail.com>
2021-09-13 15:07:07 -0400

    Final Solution

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String wi = "WI";
+        double amount = 0.0;
+        double tax = 0.0;
+        String state;
+        Scanner scannerUserInput= new Scanner(System.in);
+
+        System.out.print("What is the order amount?");
+        amount =  scannerUserInput.nextDouble();
+        tax = 0.055*amount;
+
+        System.out.print("What is the state?");
+        state =  scannerUserInput.next();
+
+        scannerUserInput.close();
+
+        if(wi.compareTo(state) == 0) {
+            System.out.println("The subtotal is " + roundNumber(amount));
+            System.out.println("The tax is " + roundNumber(tax));
+            System.out.println("The total is " + roundNumber(amount+tax));
+            return ;
+        }
+
+        System.out.println("The total is " + roundNumber(amount));
+
+
+
+    }
+
+    public static double roundNumber(double value) {
+        return (double)Math.round(value * 100d) / 100d;
+    }
+
+}
